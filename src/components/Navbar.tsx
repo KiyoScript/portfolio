cdef91f732321d296b4b09572a05e53baf784f7e
kiyoscript <89757149+KiyoScript@users.noreply.github.com>
2025-06-18 20:17:53 +0800

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,9 +33,11 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
-          <div className="text-2xl font-bold text-gray-900">
+          <a
+            href="https://portfolio-two-phi-htet9uqf15.vercel.app/"
+            className="text-2xl font-bold text-gray-900">
             KiyoScript<span className="text-red-500">.</span>
-          </div>
+          </a>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-8">
